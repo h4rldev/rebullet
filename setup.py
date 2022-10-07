08da022f0d88bc408dac51772e3f46e69683af73
H4rl <98224660+H4rldev@users.noreply.github.com>
2022-10-07 23:35:33 +0000

    ugh I am so tired

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
       name='bullet',
-      version='2.2.0',
+      version='2.2.1',
       description='Beautiful Python prompts made simple.',
       long_description="Extensive support for Python list prompts \
             formatting and colors",
       url='https://github.com/Mckinsey666/bullets',
       keywords = "cli list prompt customize colors",
-      author='Mckinsey666',
+      author='Mckinsey666 and Maintainers',
       license='MIT',
       packages=find_packages(),
       python_requires=">=3.6",
