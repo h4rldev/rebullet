b335730d8e2cd44e2f33431cef60e2f559712b8a
h4rl <0derp05@gmail.com>
2022-10-16 01:44:27 +0200

    i'm very stupid

--- bullet/cursor.py
+++ bullet/cursor.py
@@ -3,7 +3,7 @@ from contextlib import contextmanager
 
 # Show and hide cursors
 
-if os.platform == 'nt':
+if os.system == 'nt':
     import msvcrt
     import ctypes
 
@@ -20,23 +20,23 @@ def hide():
         _show_cursor()
 
 def _hide_cursor():
-    if os.platform == 'nt':
+    if os.system == 'nt':
         ci = _CursorInfo()
         handle = ctypes.windll.kernel32.GetStdHandle(-11)
         ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
         ci.visible = False
         ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
-    elif os.platform == 'posix':
+    elif os.system == 'posix':
         sys.stdout.write("\033[?25l")
         sys.stdout.flush()
 
 def _show_cursor():
-    if os.platform == 'nt':
+    if os.system == 'nt':
         ci = _CursorInfo()
         handle = ctypes.windll.kernel32.GetStdHandle(-11)
         ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
         ci.visible = True
         ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
-    elif os.platform == 'posix':
+    elif os.system == 'posix':
         sys.stdout.write("\033[?25h")
         sys.stdout.flush()
